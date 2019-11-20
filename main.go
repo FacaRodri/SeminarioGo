ae778f99ed17faf6fd3f4a0b725c5071fd02197f
Facundo Rodriguez Eseverri <facundo.eseverri@gmail.com>
2019-11-20 18:41:44 -0300

    API funcional

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
+
 import (
-	"fmt"
 	
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"seminario/otracarpeta"
+
+	"github.com/gin-gonic/gin"
 )
-
-
 
 func main() {
 	fmt.Printf("quien?\n")
 
-	c := otracarpeta.SumarValores(4, 5) 
+	c := otracarpeta.SumarValores(4, 5)
 	auto1 := otracarpeta.NewAuto("1A", "Verde", "Ford")
 	auto2 := otracarpeta.NewAuto("2B", "Gris", "Ford")
 	auto3 := otracarpeta.NewAuto("3C", "Plateado", "VMW")
@@ -26,4 +29,41 @@ func main() {
 	agencia.EditColor("1A", "Verde Claro")
 	fmt.Println(agencia.GetAutos())
 	fmt.Println(c)
+
+	//Punteros
+	var m *int
+	n := 10
+	l := 20
+	m = &n
+	fmt.Println(n)
+	fmt.Println(m)
+	fmt.Println(l)
+	fmt.Println(&l)
+
+	//Json
+	myString := "Hola como andas"
+
+	fmt.Println("MyString", myString)
+
+	miAuto := auto1
+
+	miAutoMarshalled, _ := json.Marshal(miAuto)
+
+	fmt.Println("MiAutoMarshalled", miAutoMarshalled)
+
+	//Api
+	router := gin.Default()
+
+	router.GET("/hello", GetHello)
+
+	if err := router.Run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func GetHello(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "hello world",
+	})
 }
